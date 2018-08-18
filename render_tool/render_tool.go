b9af502a3cef366b10188325d6dc8d1451ddd8ea
Mihai Todor <todormihai@gmail.com>
2018-08-18 21:36:17 +0100

    Add raster to SVG in the render tool

--- render_tool/render_tool.go
+++ render_tool/render_tool.go
@@ -10,37 +10,83 @@ import (
 	"github.com/Nitro/lazypdf"
 )
 
+type rasterType int
+
+const (
+	RasterPNG rasterType = iota
+	RasterSVG
+)
+
 var (
 	pdf   = kingpin.Arg("pdf", "PDF file").Required().String()
 	page  = kingpin.Flag("page", "page").Default("1").Short('p').Int()
 	size  = kingpin.Flag("size", "size").Default("0").Short('s').Int()
 	scale = kingpin.Flag("scale", "scale").Default("1.5").Short('S').Float()
 	out   = kingpin.Flag("out", "out").Default("").Short('o').String()
+	ext   = kingpin.Flag("ext", "ext").Default("png").Short('e').String()
 )
+
+func getRasterType(ext string) rasterType {
+	switch ext {
+	case "png":
+		return RasterPNG
+	case "svg":
+		return RasterSVG
+	}
+
+	return RasterPNG
+}
+
+func outputWriter(writer func(f *os.File)) {
+	f, err := os.Create(*out)
+	if err != nil {
+		log.Fatalf("IO error: %s", err)
+	}
+	defer f.Close()
+
+	writer(f)
+}
 
 func main() {
 	kingpin.Parse()
 
 	if *out == "" {
-		*out = *pdf + ".png"
+		*out = *pdf + "." + *ext
 	}
 
 	raster := lazypdf.NewRasterizer(*pdf)
 	err := raster.Run()
 	if err != nil {
-		log.Fatalf("Failed to initialize the renderer: %s", err)
+		log.Fatalf("Failed to initialize the rasteriser: %s", err)
 	}
 
-	img, err := raster.GeneratePageImage(*page, *size, *scale)
-	if err != nil {
-		log.Fatalf("Render error: %s", err)
-	}
+	switch getRasterType(*ext) {
+	case RasterPNG:
+		img, err := raster.GeneratePageImage(*page, *size, *scale)
+		if err != nil {
+			log.Fatalf("Raster error: %s", err)
+		}
 
-	f, err := os.Create(*out)
-	if err != nil {
-		log.Fatalf("IO error: %s", err)
-	}
+		outputWriter(func(f *os.File) {
+			err = png.Encode(f, img)
+			if err != nil {
+				log.Fatalf("Failed to write image to file: %s", err)
+			}
+		})
 
-	defer f.Close()
-	png.Encode(f, img)
+	case RasterSVG:
+		svgData, err := raster.GeneratePageSVG(*page, *size, *scale)
+		if err != nil {
+			log.Fatalf("Raster error: %s", err)
+		}
+
+		outputWriter(func(f *os.File) {
+			_, err = f.WriteString(svgData)
+			if err != nil {
+				log.Fatalf("Failed to write SVG to file: %s", err)
+			}
+		})
+	default:
+		log.Fatalf("Can't raster to: %s", *ext)
+	}
 }
