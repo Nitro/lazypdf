d983f05d609c6813494edbb6a104b1cb5c4ade0c
Diego Bernardes <diego.bernardes@gonitro.com>
2021-05-30 10:08:09 +0100

    Lazyraster V2 (#39)
    * Lazyraster V2
    
    At the V2 version, we're focusing solely on the features needed by Nitro
    and moving all the logic back to C with Go being used just as a thin
    integration layer.
    
    * Update MuPDF library for macos
    
    Co-authored-by: GitHub Actions <no-reply@github.com>

---
+++ main.go
@@ -0,0 +1,73 @@
+package lazypdf
+
+/*
+#cgo CFLAGS: -I ${SRCDIR}/misc/mupdf/include -I ${SRCDIR}/misc/mupdf/include/mupdf
+#cgo darwin,amd64 LDFLAGS: -L ${SRCDIR}/misc/mupdf/lib/x86-64-macos -lmupdf -lmupdf-third
+#cgo linux,amd64 LDFLAGS: -L ${SRCDIR}/misc/mupdf/lib/x86-64-linux -lmupdf -lmupdf-third -lm
+#include "main.h"
+*/
+import "C"
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"sync"
+	"time"
+
+	ddTracer "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+)
+
+// Global state to interact with C.
+// nolint: gochecknoglobals
+var (
+	baseCtx           context.Context
+	baseCtxCancel     func()
+	cancelationReason string
+	mutex             sync.Mutex
+)
+
+// SaveToPNG is used to convert a page from a PDF file to PNG.
+func SaveToPNG(ctx context.Context, page, width uint16, scale float32, rawPayload io.Reader, output io.Writer) error {
+	span, ctx := ddTracer.StartSpanFromContext(ctx, "Client.SaveToPNG")
+	defer span.Finish()
+
+	if rawPayload == nil {
+		return errors.New("payload can't be nil")
+	}
+	if output == nil {
+		return errors.New("output can't be nil")
+	}
+
+	t1 := time.Now()
+	mutex.Lock()
+	span.SetTag("LockContention", time.Since(t1))
+	defer mutex.Unlock()
+
+	payload, err := io.ReadAll(rawPayload)
+	if err != nil {
+		return fmt.Errorf("fail to read the payload: %w", err)
+	}
+	payloadPointer := C.CBytes(payload)
+	defer C.free(payloadPointer)
+
+	baseCtx, baseCtxCancel = context.WithCancel(ctx)
+	result := C.save_to_png(C.int(page), C.int(width), C.float(scale), (*C.uchar)(payloadPointer), C.size_t(len(payload)))
+	if baseCtx.Err() != nil {
+		return errors.New(cancelationReason)
+	}
+	defer C.drop_result(result)
+
+	png := C.GoStringN(result.data, C.int(result.len))
+	if _, err = output.Write([]byte(png)); err != nil {
+		return fmt.Errorf("fail to write the result at the output: %w", err)
+	}
+	return nil
+}
+
+//export errorHandler
+func errorHandler(message *C.cchar) {
+	baseCtxCancel()
+	cancelationReason = C.GoString(message)
+}
---
+++ main_test.go
@@ -0,0 +1,67 @@
+package lazypdf
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestClientSaveToPNGOK(t *testing.T) {
+	for i := uint16(0); i < 13; i++ {
+		file, err := os.Open("testdata/sample.pdf")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, file.Close()) }()
+
+		buf := bytes.NewBuffer([]byte{})
+		err = SaveToPNG(context.Background(), i, 0, 0, file, buf)
+		require.NoError(t, err)
+
+		expectedPage, err := ioutil.ReadFile(fmt.Sprintf("testdata/sample_page%d.png", i))
+		require.NoError(t, err)
+		resultPage, err := io.ReadAll(buf)
+		require.NoError(t, err)
+		require.Equal(t, expectedPage, resultPage)
+	}
+}
+
+func TestSaveToPNGFail(t *testing.T) {
+	file, err := os.Open("testdata/sample-invalid.pdf")
+	require.NoError(t, err)
+	defer func() { require.NoError(t, file.Close()) }()
+
+	err = SaveToPNG(context.Background(), 0, 0, 0, file, nil)
+	require.Error(t, err)
+}
+
+func BenchmarkSaveToPNGPage0(b *testing.B)  { benchmarkSaveToPNGRunner(0, b) }
+func BenchmarkSaveToPNGPage1(b *testing.B)  { benchmarkSaveToPNGRunner(1, b) }
+func BenchmarkSaveToPNGPage2(b *testing.B)  { benchmarkSaveToPNGRunner(2, b) }
+func BenchmarkSaveToPNGPage3(b *testing.B)  { benchmarkSaveToPNGRunner(3, b) }
+func BenchmarkSaveToPNGPage4(b *testing.B)  { benchmarkSaveToPNGRunner(4, b) }
+func BenchmarkSaveToPNGPage5(b *testing.B)  { benchmarkSaveToPNGRunner(5, b) }
+func BenchmarkSaveToPNGPage6(b *testing.B)  { benchmarkSaveToPNGRunner(6, b) }
+func BenchmarkSaveToPNGPage7(b *testing.B)  { benchmarkSaveToPNGRunner(7, b) }
+func BenchmarkSaveToPNGPage8(b *testing.B)  { benchmarkSaveToPNGRunner(8, b) }
+func BenchmarkSaveToPNGPage9(b *testing.B)  { benchmarkSaveToPNGRunner(9, b) }
+func BenchmarkSaveToPNGPage10(b *testing.B) { benchmarkSaveToPNGRunner(10, b) }
+func BenchmarkSaveToPNGPage11(b *testing.B) { benchmarkSaveToPNGRunner(11, b) }
+func BenchmarkSaveToPNGPage12(b *testing.B) { benchmarkSaveToPNGRunner(12, b) }
+
+func benchmarkSaveToPNGRunner(page uint16, b *testing.B) {
+	buf, err := ioutil.ReadFile("testdata/sample.pdf")
+	require.NoError(b, err)
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		input := bytes.NewBuffer(buf)
+		output := bytes.NewBuffer([]byte{})
+		err := SaveToPNG(context.Background(), page, 0, 0, input, output)
+		require.NoError(b, err)
+	}
+}
