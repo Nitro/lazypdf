2d5e2af28649b509e0f3579f53ed2a3bcf753ea6
Roman Lasskiy <rlasskiy@gmail.com>
2025-09-16 09:49:23 +0100

    wip

---
+++ pdf_handler_pagecount_test.go
@@ -0,0 +1,36 @@
+package lazypdf
+
+/*
+import (
+    "bytes"
+    "context"
+
+    "fmt"
+    "io"
+    "log/slog"
+    "os"
+    "testing"
+
+    "github.com/stretchr/testify/require"
+)
+
+func TestPageCount(t *testing.T) {
+    t.Parallel()
+
+    file, err := os.Open("testdata/sample.pdf")
+    require.NoError(t, err)
+    defer func() { require.NoError(t, file.Close()) }()
+
+    handler := setupPdfHandler(t)
+
+    document, err := handler.OpenPDF(file)
+    if err != nil {
+        t.Fatalf("OpenPDF: %v", err)
+    }
+    defer func() { require.NoError(t, handler.ClosePDF(document)) }()
+
+    count, err := handler.PageCount(document)
+    require.NoError(t, err)
+    require.Equal(t, 1, count)
+}
+*/
